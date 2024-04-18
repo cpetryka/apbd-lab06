96e531502cfaaabfde9a53e682ad766ad5f4db05
Cezary Petryka <cezaryp10@gmail.com>
2024-04-18 20:57:38 +0200

    Add HTTP GET method to AnimalsController

--- Tutorial6/Tutorial6/Controller/AnimalsController.cs
+++ Tutorial6/Tutorial6/Controller/AnimalsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Tutorial6.Model;
 
 namespace Tutorial6.Controller;
 
@@ -11,5 +13,53 @@ public class AnimalsController : ControllerBase
     public AnimalsController(IConfiguration configuration)
     {
         _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals([FromQuery] string orderBy = "name")
+    {
+        // Lista dozwolonych kolumn do sortowania
+        var allowedOrderByValues = new HashSet<string>{"name", "description", "category", "area"};
+
+        // Ustawienie domyślnego sortowania, jeśli przekazany parametr jest nieprawidłowy
+        if (!allowedOrderByValues.Contains(orderBy))
+        {
+            orderBy = "name";
+        }
+
+        // Otwieramy połączenie
+        using var connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+
+        // Definiujemy command
+        using var command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = $"SELECT * FROM Animal ORDER BY {orderBy} ASC";
+
+        // Wykonujemy zapytanie
+        var reader = command.ExecuteReader();
+
+        // Przetwarzamy wynik zapytania
+        var animals = new List<Animal>();
+
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        int descriptionOrdinal = reader.GetOrdinal("Description");
+        int categoryOrdinal = reader.GetOrdinal("Category");
+        int areaOrdinal = reader.GetOrdinal("Area");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal),
+                Description = reader.IsDBNull(descriptionOrdinal) ? null : reader.GetString(descriptionOrdinal),
+                Category = reader.GetString(categoryOrdinal),
+                Area = reader.GetString(areaOrdinal)
+            });
+        }
+
+        return Ok(animals);
     }
 }
